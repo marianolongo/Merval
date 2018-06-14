3b5c5d5a37e7ebb41c19b3e78ef6efa5274a6367
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2018-06-14 18:04:12 -0300

    2.1

--- Test.py
+++ Test.py
@@ -22,21 +22,14 @@ class Test:
         titles_element = self.driver.find_elements_by_xpath("//article[contains(@class,'portada-article-titulo')]")
         href_list = [title.find_element_by_css_selector('a').get_attribute('href') for title in titles_element]
         for i in range(0, href_list.__len__() - 1):
-            self.driver.set_script_timeout(5)
-            try:
-                self.driver.get(href_list[i])
-                title = self.driver.find_element_by_xpath("//header[contains(@class,'titulo-noticia')]")
-                title_text = title.text
-                body = self.driver.find_element_by_xpath(
-                    "//article[contains(@class,'container-fluid despliegue-noticia')]")
-                body_text = body.text
-            except TimeoutError:
-                self.driver.execute_script("window.stop();")
-            f = open(title.text + ".txt", "w")
-            f.write("Title: " + title.text + '\n')
-            f.write("Body" + '\n' + body.text)
-            f.close()
-            sleep(5)
+            self.driver.get(href_list[i])
+            title = self.driver.find_element_by_xpath("//header[contains(@class,'titulo-noticia')]")
+            body = self.driver.find_element_by_xpath(
+                "//article[contains(@class,'container-fluid despliegue-noticia')]")
+            with open(title.text + '.txt', 'w+') as file:
+                file.write(title.text)
+                file.write(body.text)
+                file.flush()
         self.driver.close()
 
 
